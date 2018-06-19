349c005aac46e5ad92eb85e0fac111197c79d770
Joseph Weber <joseph.theodore.weber@gmail.com>
2018-06-19 11:49:06 -0400

    renamed global variables for scalability

--- js/app.js
+++ js/app.js
@@ -8,16 +8,40 @@ const $conanButton = $('.conan-button');
 
 const $guyButton = $('.guy-button');
 
+const $guyHealth = $('#guy-health');
+
+const $conanHealth = $('#conan-health');
+
+const $guyImage = $('#guy-pic');
+
+const $conanImage = $('#conan-pic');
+
+const $guyShield = $('#guy-shield');
+
+const $conanShield = $('#conan-shield');
+
+const $attackText = $('#attack-text');
+
+const $attackImage = $('#attack');
+
 
 
 //////////////////////////////////////////
 // All my functions go here
+// Win game function
 const winGame = (opponent) => {
   if (opponent.health <= 0){
-    alert('you won the game');
+    $attackText.text('Guy Fieri is victorious. Ed Hardy decals for everyone');
+    $conanImage.css('transform', 'rotate(180deg)')
   }
 }
-
+// Lose game function
+const loseGame = (opponent) => {
+  if (opponent.health <= 0){
+    $attackText.text('Guy Fieri has been defeated. Norm-core wins!');
+    $guyImage.css('transform', 'rotate(180deg)')
+  }
+}
 
 //////////////////////////////////////////
 // Create my general fighter class here
@@ -25,16 +49,21 @@ class Character {
   // health will be the same for both characters
   constructor(name, health){
     this.name = name
-    this.health = health
+    this.health = 400
   }
   // Attack methods that must do accuracy function, draw away health if hit, log message, and lead to an animation
   attack(opponent, move){
-    console.log(opponent.health);
   }
   // method to allow character to guard and gain some health
   shield(){
-    console.log('shield');
-  }
+    const shieldRando = Math.random() * 1;
+    if (shieldRando > .5){
+      $attackText.text('your defense has been bolstered by 50 hp');
+    }
+    else {
+      $attackText.text('your shields short-circuited, no dice');
+    }
+}
 }
 
 /////////////////////////////////////////
@@ -66,52 +95,56 @@ class TVChef extends Character {
   // give message about partic attack
   attack(opponent, move){
   super.attack(opponent, move)
-  const rando = Math.random() * 1;
+  const guyRando = Math.random() * 1;
   switch (move) {
   case 'Donkey Sauce':
-    if (rando < this.weapons.donkeySauce.accuracy){
-    console.log('You have dealt 25 damage to ' + opponent.name);
+    if (guyRando < this.weapons.donkeySauce.accuracy){
+    $attackText.text('You have dealt 25 damage to ' + opponent.name);
     opponent.health -= 25;
   }
     else {
-      console.log('you missed');
+      $attackText.text('you missed');
     }
     break;
   case 'Frosted Tip Projectiles':
-    if (rando < this.weapons.frostedTipProjectiles.accuracy){
-    console.log('You have dealt 10 damage to ' + opponent.name);
+    if (guyRando < this.weapons.frostedTipProjectiles.accuracy){
+    $attackText.text('You have dealt 10 damage to ' + opponent.name);
     opponent.health -= 10;
     }
     else {
-      console.log('you missed');
+      $attackText.text('you missed');
+
     }
     break;
   case 'Bowling Shirt Flamethrower':
-  if (rando < this.weapons.bowlingShirtFlameThrower.accuracy){
-    console.log('You have dealt 100 damage to ' + opponent.name);
+  if (guyRando < this.weapons.bowlingShirtFlameThrower.accuracy){
+    $attackText.text('You have dealt 100 damage to ' + opponent.name);
     opponent.health -= 100;
     }
     else {
-    console.log('you missed');
+    $attackText.text('you missed');
   }
   break;
   case 'Alliterative TV Show':
-  if (rando < this.weapons.alliterativeShowTitles.accuracy){
-    console.log('You have dealt 40 damage to ' + opponent.name);
+  if (guyRando < this.weapons.alliterativeShowTitles.accuracy){
+    $attackText.text('You have dealt 40 damage to ' + opponent.name);
     opponent.health -= 40;
     }
     else {
-    console.log('you missed');
+    $attackText.text('you missed');
   }
   break;
 }
 winGame(wAA);
 $conanButton.show();
 $guyButton.hide();
+$conanHealth.text(wAA.health);
 }
   // give message about guard
   shield () {
-super.shield ()
+super.shield ();
+$guyButton.hide();
+$conanButton.show();
 }
 }
 
@@ -143,46 +176,72 @@ class Enemy extends Character {
   }
   // give message about partic attack
   attack(opponent, move){
-  super.attack(opponent, move)
+  super.attack(opponent, move);
+  const conanRando = Math.random() * 1;
   switch (move) {
   case 'Health Conscious Food':
-    console.log('You have dealt 25 damage to ' + opponent.name);
+  if (conanRando < this.weapons.healthConsciousFood.accuracy){
+    $attackText.text('You have dealt 25 damage to ' + opponent.name);
     opponent.health -= 25;
+  }
+  else {
+    $attackText.text('you missed');
+  }
     break;
   case 'Appropriate Clothing':
-  console.log('You have dealt 10 damage to ' + opponent.name);
-  opponent.health -= 10;
+  if (conanRando < this.weapons.appropriateClothing.accuracy){
+    $attackText.text('You have dealt 10 damage to ' + opponent.name);
+    opponent.health -= 10;
+  }
+  else {
+    $attackText.text('you missed');
+  }
   break;
   case 'Mid-life Crisis Antidote':
-    console.log('You have dealt 100 damage to ' + opponent.name);
+  if (conanRando < this.weapons.midlifeCrisisAntidote.accuracy){
+    $attackText.text('You have dealt 100 damage to ' + opponent.name);
     opponent.health -= 100;
+  }
+  else {
+    $attackText.text('you missed');
+  }
     break;
   case 'Good Table Manners':
-    console.log('You have dealt 40 damage to ' + opponent.name);
+  if (conanRando < this.weapons.goodTableManners.accuracy){
+    $attackText.text('You have dealt 40 damage to ' + opponent.name);
     opponent.health -= 40;
+  }
+  else {
+    $attackText.text('you missed');
+  }
     break;
 }
+loseGame(guy);
 $conanButton.hide();
 $guyButton.show();
+$guyHealth.text(guy.health);
 }
   // give message about guard
   shield () {
 super.shield ()
+$conanButton.hide();
+$guyButton.show();
   }
 }
 
-const guy = new TVChef('Guy', 1000);
+const guy = new TVChef('Guy');
 
-const wAA = new Enemy('Well Adjusted Adult', 1000);
+const wAA = new Enemy('Well Adjusted Adult');
 
-console.log(guy.weapons.donkeySauce.power);
 
 // commented this out for practice console.log(wAA.weapons.midlifeCrisisAntidote.power);
 
 // guy.attack(wAA, 'Frosted Tip Projectiles');
-guy.shield();
-wAA.shield();
 $(()=> {
+  /// Display score
+
+
+  /// All my onclick events
   $("#donkey-sauce").on('click', ()=>{guy.attack(wAA, $(event.currentTarget).text())
   });
   $("#alliterative-tv-show").on('click', ()=>{guy.attack(wAA, $(event.currentTarget).text())
@@ -203,7 +262,14 @@ $(()=> {
   $("#good-table-manners").on('click', ()=>{
     wAA.attack(guy, $(event.currentTarget).text())
   });
+  $guyShield.on('click', () => {
+    guy.shield()
+  });
+  $conanShield.on('click', () => {
+    wAA.shield()
+  });
 
+// $guyHealth.text(wAA.health);
 
   // on click events for each of the attacks
   // we will draw the html text from the event current target to get the input
